4be51730d3626933fe5646c8e339da4399073be3
nanbuwks <nanbuwks+github@nanbu.com>
2024-06-25 07:49:02 +0900

    make progresscheck function

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # SETUP
 
-please make .env file from env_example
+please make some files
+
+- .env file from env_example
+- node.json file from node_example.json
---
+++ progresscheck_bot.py
@@ -0,0 +1,56 @@
+import datetime
+import hashlib
+import json
+import requests
+import discord
+import os
+from dotenv import load_dotenv
+import time
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
+node_open = open('node.json', 'r')
+saved_open = open('saved.json', 'r')
+node_load = json.load(node_open)
+saved_load = json.load(saved_open)
+newsavefile = ('saved.json')
+new_save_contents = {}
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('ready on')
+    channel = client.get_channel(CHANNEL_ID)
+    for key,value in node_load.items():
+        dt_now = datetime.datetime.now()
+        # print(value["user"])
+        # print(value["title"])
+        # print(saved_load[key]["hash"])
+        urlData = requests.get(value["checkurl"]).content
+        # print(urlData)
+        time.sleep(5)
+        m = hashlib.sha256()
+        m.update(urlData)
+        # print(m.hexdigest())
+        datestr = dt_now.strftime('%Y%m%d %H%M')
+        hash = m.hexdigest()
+        if (hash == saved_load[key]["hash"]):
+            save_node = {"hash":hash,"date": saved_load[key]["date"] }
+            # print(value["user"],value["title"],saved_load[key]["date"],"進捗ありません")
+            sendstr = "進捗ないです",saved_load[key]["date"],value["user"],value["title"]
+            await channel.send(sendstr)
+        else:
+            save_node = {"hash":hash,"date": datestr }
+            # print(value["user"],value["title"],datestr,"更新ありました")
+            sendstr = "捗ってますね",datestr,value["user"],value["title"]
+            await channel.send(sendstr)
+        new_save_contents[key] = save_node
+    # await channel.send('進捗どうですか')
+
+    with open(newsavefile, mode="wt", encoding="utf-8" )as f:
+         json.dump(new_save_contents,f,ensure_ascii=False,indent=2)
+
+    await client.close()
+client.run(TOKEN)
